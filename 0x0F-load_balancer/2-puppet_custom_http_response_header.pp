849dbec2a23ab9a76ba2d5c1316226e0a700ddf8
Adejumo Adeyemi <adejumoadeyemi32@gmail.com>
2023-04-03 18:36:46 -0700

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,25 @@
-# Automation: creates a custom HTTP header response with Puppet.
-exec { 'command':
-  command  => 'apt-get -y update;
-  apt-get -y install nginx;
-  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-  sudo service nginx restart
+# Installs a Nginx server with custome HTTP header
+
+exec {'update':
   provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
+}
+
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
+}
+
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i '0,/location \/ {/ s//location \/ {\n\tadd_header X-Served-By $HOST;\n/' /etc/nginx/sites-available/default',
+  before      => Exec['restart Nginx'],
+}
+
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
