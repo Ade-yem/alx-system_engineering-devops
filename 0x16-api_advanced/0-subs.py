c6400fb65672f3430c3d32e6fa13b92cadfba885
Adeyemi Adejumo <93265896+Ade-yem@users.noreply.github.com>
2023-05-09 22:58:22 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    r = requests.get(f'http://www.reddit.com/r/{subreddit}/about.json',
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
                      headers={'User-Agent': 'Mozilla/5.0'}).json()
     subs = r.get("data", {}).get("subscribers", 0)
     return subs
