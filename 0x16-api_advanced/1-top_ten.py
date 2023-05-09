95a9733a7be3c6964545a5e4d0463b4e5e2a88af
Adeyemi Adejumo <93265896+Ade-yem@users.noreply.github.com>
2023-05-09 23:16:35 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,9 @@ def top_ten(subreddit):
     titles of the first 10 hot posts listed for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
-    request = requests.get(url, headers=headers)
+    params = {"limit": 10}
+    request = requests.get(url, headers=headers, params=params,
+                           allow_redirects=False)
     if request.status_code == 200:
         req = request.json().get("data")
         [print(c.get("data").get("title")) for c in req.get("children")]
