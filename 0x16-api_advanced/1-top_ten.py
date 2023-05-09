4d5c1a60d46b67c4216655b4ccbb06d2a35e0b09
Adeyemi Adejumo <93265896+Ade-yem@users.noreply.github.com>
2023-05-09 23:05:01 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ import requests
 def top_ten(subreddit):
     """function that queries the Reddit API and prints the
     titles of the first 10 hot posts listed for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
     request = requests.get(url, headers=headers)
     if request.status_code == 200:
